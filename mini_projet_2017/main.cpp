4cd6e8d2e0806fb716ebd5147a3fa713ec0606bd
tastyapp <antoine.beneteau@efrei.net>
2017-01-03 00:59:44 +0100

    Initial Commit

---
+++ mini_projet_2017/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mini_projet_2017
+//
+//  Created by antoine beneteau on 2017-01-03.
+//  Copyright © 2017 antoine beneteau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
