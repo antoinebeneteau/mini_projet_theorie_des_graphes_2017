640ce462dd4f646121c28341c25023162abebd01
Antoine MALINUR <antoine.malinur@gmail.com>
2017-01-03 01:41:30 +0100

    essai

--- mini_projet_2017/main.cpp
+++ mini_projet_2017/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  mini_projet_2017
-//
-//  Created by antoine beneteau on 2017-01-03.
-//  Copyright © 2017 antoine beneteau. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    printf("Salut c'est coooooooooool");
-    return 0;
-}
+essaiiii
